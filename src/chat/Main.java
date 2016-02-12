aca07fef4a488b9eafba94c3fa33ac7121276377
AlexanderSergeev <asergeev1997@yandex.ru>
2016-02-12 20:51:56 +0300

    Something works

--- src/chat/Main.java
+++ src/chat/Main.java
@@ -1,18 +1,15 @@
 package chat;
 
-
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 
-
-
 public class Main {
 
-	public static void main(String[] args) throws XMPPException  {
+	public static void main(String[] args) throws XMPPException {
 		// Create the configuration for this new connection
-		ConnectionConfiguration config = new ConnectionConfiguration("jabber.com");
+		ConnectionConfiguration config = new ConnectionConfiguration("jabber.ru");
 		config.setCompressionEnabled(true);
 		config.setSASLAuthenticationEnabled(true);
 
@@ -20,11 +17,9 @@ public class Main {
 		// Connect to the server
 		connection.connect();
 		// Log into the server
-		connection.login("Alexander_Sergeev", "ManuelNeuer", "jabber.ru");
+		connection.login("Alexander_Sergeev@jabber.ru", "ManuelNeuer", "jabber.ru");
 		// Disconnect from the server
 		connection.disconnect();
-
-
-
 	}
+
 }
